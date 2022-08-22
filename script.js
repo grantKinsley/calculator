9b3dacac9239dcc9e719262aa64ab6da55b3d4fe
Grant Kinsley <grantkinsley@gmail.com>
2022-08-22 00:25:58 -0700

    binded buttons to arithmetic functions

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let operandA = 0;
+let operandB = 0;
+let currentOperator = '';
+let displayText = "";
+
 const add = (a, b) => {
     return a + b;
 }
@@ -29,3 +34,35 @@ const operate = (operator, a, b) => {
             return divide(a, b);
      }
 }
+
+function bindButtons() {
+    const digits = document.querySelectorAll('.digit');
+    const display = document.querySelector('.display');
+
+    digits.forEach(digit => {
+        digit.addEventListener('click', () => {
+            displayText += digit.textContent;  
+            display.textContent = displayText;
+        });
+    });
+
+    const operators = document.querySelectorAll('.operator');
+
+    operators.forEach(operator => {
+        operator.addEventListener('click', () => {
+            currentOperator = operator.textContent;
+            displayText += ` ${currentOperator} `;
+            display.textContent = displayText;
+        })
+    })
+
+    const equalSign = document.querySelector('.equal');
+
+    equalSign.addEventListener('click', () => {
+        const result = operate(currentOperator, operandA, operandB)
+        displayText.textContent = result;
+        console.log(currentOperator, operandA, operandB)
+    })
+}
+
+bindButtons();
