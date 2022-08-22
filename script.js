8be800385d313797dbe214b08df8bcaf80077a72
Grant Kinsley <grantkinsley@gmail.com>
2022-08-21 19:01:43 -0700

    created basic arithmetic functions

---
+++ script.js
@@ -0,0 +1,31 @@
+const add = (a, b) => {
+    return a + b;
+}
+
+const subtract = (a, b) => {
+    return a - b;
+}
+
+const multiply = (a, b) => {
+    return a * b;
+}
+
+const divide = (a, b) => {
+    if (b === 0) {
+        return undefined;
+    }
+    return a / b;
+}
+
+const operate = (operator, a, b) => {
+     switch(operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+     }
+}
